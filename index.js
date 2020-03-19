57793bc760ade5479f4f46f0b052878c930df2e4
arezki-max <faridd.arezki@yahoo.fr>
2020-03-19 11:02:50 +0100

    ajout com

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("arezki");
+let variable1=1;
+//ggggggggggggggggggggg
+console.log("variable1:",1);
+	let variable3="farid";
+console.log(variable3);
+function calcule(arg1,arg2){
+const resultat = arg1*arg2;
+return resultat;
+}
+const calc=(5,10);
+console.log(resultat);
