e5c1ebed1ff63c55597af4e1a8e237d363bc1d27
arezki-max <faridd.arezki@yahoo.fr>
2020-03-19 08:51:27 +0100

    un structure.js

---
+++ structure.js
@@ -0,0 +1,49 @@
+/* myData() { 
+	let result= 10*5;
+   return result; 
+}
+
+let newmyData=myData();
+console.log(myData()); // 123 
+
+function myValue() { 
+ let num="farida";
+ return num;
+} 
+console.log(myValue()); // undefined 
+
+
+function hello(name,prenom,arg1) {
+   console.log(name,prenom,arg1 );
+}
+ 
+hello("julia","arezki","va à l'école");
+
+console.log(newmyData*/
+function percentage(a,b) {
+	let calcule= a/100*b;
+	return ;
+	// body...
+}
+percentage(10,100)
+console.log(percentage());
+
+var user = [
+     first_name= "HTML",
+     last_name= "CSS",
+     age= 32,
+     website= "java2s.com"
+
+  first1_name= "HTML",
+     last1_name= "CSS",
+     age1= 32,
+     website1= "java2s.com"
+];
+
+
+
+
+
+for (var i = 0; i <user.length; i++) {
+	console.log (user[i]);
+}
